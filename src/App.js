93c016890a462ecf6700f5895bb644d1ae5263e2
Crystal Csete <jeannecc13@gmail.com>
2021-05-13 20:06:05 -0600

    add ant design to app file and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./App.css";
-import { Layout, Typography, Avatar, Menu, Breadcrumb } from "antd";
-import { HeartOutlined, MailOutlined } from "@ant-design/icons";
+import {
+  Layout,
+  Typography,
+  Avatar,
+  Image,
+  Menu,
+  Breadcrumb,
+  Card,
+} from "antd";
+import {
+  HeartOutlined,
+  UserOutlined,
+  GithubOutlined,
+  MediumWorkmarkOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
+import user from "./assets/user.jpg";
 
 const App = () => {
   const { Header, Footer, Sider, Content } = Layout;
@@ -9,12 +24,13 @@ const App = () => {
   const { SubMenu } = Menu;
 
   return (
-    <div className="App">
+    <div className="app__container">
       <Layout>
         <Header style={{ padding: 10 }}>
-          <Avatar style={{ float: "right" }} icon={<HeartOutlined />} />
-          <Title style={{ color: "#fff" }} level={3}>
-            Crystal
+          <Title style={{ color: "rgb(194, 57, 164)" }} level={3}>
+            <Avatar style={{ float: "right" }} icon={<HeartOutlined />} />
+
+            {/* <Avatar src={<Image src={user} />} /> */}
           </Title>
         </Header>
         <Layout>
@@ -24,31 +40,63 @@ const App = () => {
               <SubMenu
                 title={
                   <span>
-                    <MailOutlined />
-                    <span>About Us</span>
+                    <UserOutlined />
+                    <span>About Me</span>
                   </span>
                 }
               >
-                <Menu.ItemGroup key="aboutus" title="Country">
-                  <Menu.Item key="location1">Location 1</Menu.Item>
-                  <Menu.Item key="location2">Location 2</Menu.Item>
+                <Menu.ItemGroup key="aboutme" title="Portfolio">
+                  <Menu.Item key="portfolio1">Portfolio Item 1</Menu.Item>
+                  <Menu.Item key="portfolio2">Portfolio Item 2</Menu.Item>
                 </Menu.ItemGroup>
               </SubMenu>
             </Menu>
           </Sider>
           <Layout>
-            <Content style={{ padding: "0 50px" }}>
+            <Content className="app__content" style={{ padding: "0 50px" }}>
               <Breadcrumb style={{ margin: "16px 0" }}>
                 <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
               </Breadcrumb>
-              <div
-                style={{
-                  backgroundColor: "#fff",
-                  padding: 24,
-                  minHeight: "100vh",
-                }}
-              >
-                Content
+              <div className="app__card__container">
+                <Card className="app__card image">
+                  <Avatar
+                    shape="square"
+                    size={200}
+                    src={<Image src={user} />}
+                  />
+                </Card>
+                <Card className="app__card">
+                  <Title level={3}>Crystal Csete</Title>
+
+                  <p>
+                    Welcome to my page! I am a Full Stack Web Developer. I
+                    graduated from Lambda School in April 2021. I am excited to
+                    start my developer career!
+                  </p>
+                </Card>
+                <Card className="app__card">
+                  <a
+                    href="https://github.com/crystal-csete"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <GithubOutlined />
+                  </a>
+                  <a
+                    href="https://jeannecc13.medium.com/adding-design-to-an-existing-application-738c08eb3140"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <MediumWorkmarkOutlined />
+                  </a>
+                  <a
+                    href="www.linkedin.com/in/crystal-csete"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <LinkedinOutlined />
+                  </a>
+                </Card>
               </div>
             </Content>
             <Footer style={{ textAlign: "center" }}>
